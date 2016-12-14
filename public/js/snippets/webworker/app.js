933f28998bd8369f6263dcbccee895264ad0ddb2
Chaithanya <chaithanya@theinnerhour.com>
2016-12-14 11:53:58 +0530

    first webworker test

--- public/js/snippets/webworker/app.js
+++ public/js/snippets/webworker/app.js
@@ -2,6 +2,14 @@
 if (typeof(Worker) !== "undefined") {
     // Yes! Web worker support!
     console.log("Web worker is on");
+    if (typeof(w) == "undefined") {
+    	w = new Worker("/js/snippets/webworker/worker.js");
+
+    }
+    w.onmessage = function(e){
+    	$('.counter').html(e.data);
+    };
 } else {
-    console.log("No support for web worker");
+	console.log("No support for web worker");
+	$('.counter').html("No web worker support");
 }
