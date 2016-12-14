40221128a48235030b956e18615ef2b0ae7cd295
Chaithanya <chaithanya@theinnerhour.com>
2016-12-14 12:12:49 +0530

    web worker code

--- public/js/snippets/webworker/app.js
+++ public/js/snippets/webworker/app.js
@@ -1,15 +1,31 @@
+var w;
 // Setting the web worker
-if (typeof(Worker) !== "undefined") {
-    // Yes! Web worker support!
-    console.log("Web worker is on");
-    if (typeof(w) == "undefined") {
-    	w = new Worker("/js/snippets/webworker/worker.js");
-
+function startWorker() {
+    if (typeof(Worker) !== "undefined") {
+        // Yes! Web worker support!
+        console.log("Web worker is on");
+        if (typeof(w) == "undefined") {
+            w = new Worker("/js/snippets/webworker/worker.js");
+        }
+        w.onmessage = function(e) {
+            $('.counter').html(e.data);
+        };
+    } else {
+        console.log("No support for web worker");
+        $('.counter').html("No web worker support");
     }
-    w.onmessage = function(e){
-    	$('.counter').html(e.data);
-    };
-} else {
-	console.log("No support for web worker");
-	$('.counter').html("No web worker support");
 }
+function stopWorker() {
+	w.terminate();
+	w = undefined;
+}
+
+// startWorker();
+
+$('.btn-start').on('click',function(){
+	startWorker();
+});
+
+$('.btn-stop').on('click',function(){
+	stopWorker();
+});
